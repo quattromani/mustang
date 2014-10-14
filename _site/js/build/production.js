9615d3dae5f5344b87b3a6866ea2c5bf7fed37d6
quattromani <max@maxquattromani.com>
2014-10-13 23:08:05 -0600

    added smooth scrolling

--- _site/js/build/production.js
+++ _site/js/build/production.js
@@ -195,3 +195,18 @@ function disablePopup() {
 /* ==========================================================================
     Sandbox -- Version: 0.2.2 - Updated: 7/26/2014
     ========================================================================== */
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
